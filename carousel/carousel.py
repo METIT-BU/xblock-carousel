066126b0cc85d00ec35e201a213775e9827c4e4a
hakimaksel <hakimb@bu.edu>
2014-06-30 13:59:53 -0400

    cleaning xml processing

--- carousel/carousel.py
+++ carousel/carousel.py
@@ -1,10 +1,11 @@
 import textwrap
 import urllib
+
 from lxml import etree
 from xml.etree import ElementTree as ET
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer, String, List
+from xblock.fields import Scope, List, String
 from xblock.fragment import Fragment
 
 from .utils import load_resource, render_template
@@ -13,7 +14,7 @@ from StringIO import StringIO
 
 class CarouselBlock(XBlock):
     """
-    An XBlock providing a media carousel 
+    An XBlock providing a responsive images carousel
     """
 
     display_name = String(help="This name appears in horizontal navigation at the top of the page.", 
@@ -21,35 +22,23 @@ class CarouselBlock(XBlock):
         scope=Scope.content
     )
     
-    data = String(help="",  
-       scope=Scope.content,
-       default=textwrap.dedent("""
-            <carousel>
-              <img>http://met-content.bu.edu/etr2/content/images/Slide1.JPG</img>
-              <img>http://met-content.bu.edu/etr2/content/images/Slide2.JPG</img>
-              <img>http://met-content.bu.edu/etr2/content/images/Slide3.JPG</img>
-              <video>http://www.youtube.com/watch?v=7uHeNryKUWk</video>
-              <doc>http://www.bu.edu/met-eti/files/2013/03/Final_VirtualLaboratoriesForLearning.pdf</doc>
-            </carousel>
-          """
-    ))
-    
-    data_items = List(default=[('img', 'http://met-content.bu.edu/etr2/content/images/Slide1.JPG', '100%', '625'),
-                               ('img', 'http://met-content.bu.edu/etr2/content/images/Slide2.JPG', '100%', '625'),
-                               ('img', 'http://met-content.bu.edu/etr2/content/images/Slide3.JPG', '100%', '625'), 
-                               ('video','http://www.youtube.com/watch?v=7uHeNryKUWk', '100%', '625'),
-                               ('doc', 'http%3A%2F%2Fwww.bu.edu%2Fmet-eti%2Ffiles%2F2013%2F03%2FFinal_VirtualLaboratoriesForLearning.pdf', '100%', '625')
-                              ])
+    data =  List(help="This is the representation of the data items as a list of tuples ",
+        default=[('img', 'http://met-content.bu.edu/etr2/content/images/Slide1.JPG', '100%', '625'),
+        ('img', 'http://met-content.bu.edu/etr2/content/images/Slide2.JPG', '100%', '625'),
+        ('img', 'http://met-content.bu.edu/etr2/content/images/Slide3.JPG', '100%', '625'), 
+        ('video','http://www.youtube.com/watch?v=7uHeNryKUWk', '100%', '625'),
+        ('doc', 'http%3A%2F%2Fwww.bu.edu%2Fmet-eti%2Ffiles%2F2013%2F03%2FFinal_VirtualLaboratoriesForLearning.pdf', '100%', '625')],
+        scope=Scope.content
+    )
 
     def student_view(self, context):
         """
-        View displayed to the student
-        """        
-
+        Lab view, displayed to the student
+        """
         fragment = Fragment()
 
         context = {
-            'items': self.data_items,
+            'items': self.data,
         }
 
         fragment.add_content(render_template('/templates/html/carousel.html', context))
@@ -57,19 +46,20 @@ class CarouselBlock(XBlock):
         fragment.add_css(load_resource('public/css/responsive-carousel.css'))
         fragment.add_css(load_resource('public/css/responsive-carousel.slide.css'))
         fragment.add_javascript(load_resource('public/js/responsive-carousel.js'))
-        fragment.add_css_url("https://vjs.zencdn.net/4.5.1/video-js.css")
-        fragment.add_javascript_url("https://vjs.zencdn.net/4.5.1/video.js")
+        fragment.add_css(load_resource("public/css/video-js.css"))
+        fragment.add_javascript(load_resource("public/js/video.js"))
         fragment.add_javascript(load_resource('public/js/youtube.js'))
-        
+
         return fragment
 
     def studio_view(self, context):
         """
         Studio edit view
         """
-
+        xml_data = self._build_xml(self.data)
+        
         fragment = Fragment()
-        fragment.add_content(render_template('templates/html/carousel_edit.html', {'self': self, }))
+        fragment.add_content(render_template('templates/html/carousel_edit.html', {'xml_data': xml_data, }))
         fragment.add_javascript(load_resource('public/js/jquery-ui-1.10.4.custom.js'))
         fragment.add_javascript(load_resource('public/js/carousel_edit.js'))
         fragment.initialize_js('CarouselEditBlock')
@@ -83,16 +73,9 @@ class CarouselBlock(XBlock):
 
         try:
             etree.parse(StringIO(xml_content))
-            self.data = xml_content
-            root = ET.fromstring(self.data)
-            items = []
-            for child in root:
-                if child.tag == 'doc': child.text = urllib.quote(child.text, '')
-                width = child.attrib.get('width', '100%')
-                height = child.attrib.get('height', '625')
-                items.append((child.tag, child.text, width, height))
-                
-            self.data_items = items
+            xmltree = etree.fromstring(xml_content)
+            items_list = self._get_items(xmltree)
+            self.data = items_list
 
         except etree.XMLSyntaxError as e:
             return {
@@ -103,6 +86,31 @@ class CarouselBlock(XBlock):
         return {
             'result': 'success',
         }
+
+    def _get_items(self, xmltree):
+        """
+        Helper method
+        """
+        items_elements = xmltree.getchildren()
+        items = []
+        for item_element in items_elements:
+            item_tag = item_element.tag
+            item_src = urllib.quote(item_element.get('src'), '') if item_tag == 'doc' else item_element.get('src')  
+            item_width = item_element.get('width', '100%')
+            item_height = item_element.get('height', '625')
+            items.append((item_tag, item_src, item_width, item_height))
+        
+        return items
+
+    def _build_xml(self, items_list):
+        """
+        Helper method
+        """
+        xml = etree.Element('carousel')
+        for item in items_list:
+            tag = etree.SubElement(xml, item[0], src=item[1], width=item[2], height=item[3])
+        return etree.tostring(xml, pretty_print=True)
+
 
     @staticmethod
     def workbench_scenarios():
